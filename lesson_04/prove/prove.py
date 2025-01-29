01d3203ac41b8cbc71ac1fc931bdbd673adf010f
Tyler Bartle <tman.bartleboy1@gmail.com>
2025-01-28 19:00:25 -0700

    Completed W04 Prove

--- lesson_04/prove/prove.py
+++ lesson_04/prove/prove.py
@@ -40,6 +40,7 @@ class Car():
 
     car_years = [i for i in range(1990, datetime.now().year)]
 
+    '''
     def __init__(self):
         # Make a random car
         self.model = random.choice(Car.car_models)
@@ -55,6 +56,27 @@ class Car():
     def info(self):
         """ Helper function to quickly get the car information. """
         return f'{self.make} {self.model}, {self.year}'
+    
+    '''
+    @staticmethod
+    def gen_random():
+        '''
+        Instantiating a new Car() each time, and therefore
+            - initializing possible makes, models, and years,
+            - randomizing the make, model, and year, and
+            - storing the make, model and year within self
+        seemed inefficient. The lists of possibilities are static,
+        so I figured a static method would save a little more time (and memory).
+        Bonus points, you don't even have to call .info() as it just returns a string (instead of a whole object).
+
+        * This one didn't say I can't modify it... I hope this isn't cheating :)
+        '''
+        make = random.choice(Car.car_makes)
+        model = random.choice(Car.car_models)
+        year = random.choice(Car.car_years)
+        # Sleep a little.  Last statement in this for loop - don't change
+        time.sleep(random.random() / (SLEEP_REDUCE_FACTOR))
+        return f'{make} {model}, {year}'
 
 
 class Queue251():
@@ -67,8 +89,11 @@ class Queue251():
         return len(self.__items)
 
     def put(self, item):
-        assert len(self.__items) <= 10
-        self.__items.append(item)
+        if len(self.__items) <= 10:
+            self.__items.append(item)
+        else:
+            assert "you fed me eleven cake you silly boy"
+        # I had to modify this because it wouldn't compile with the original assert line
 
     def get(self):
         return self.__items.pop(0)
@@ -77,44 +102,67 @@ class Queue251():
 class Factory(threading.Thread):
     """ This is a factory.  It will create cars and place them on the car queue """
 
-    def __init__(self):
+    def __init__(self, queue, space_available, cars_available, log):
         # TODO, you need to add arguments that will pass all of data that 1 factory needs
         # to create cars and to place them in a queue.
-        pass
+        super().__init__()
+        self.queue = queue
+        self.space_available = space_available
+        self.cars_available = cars_available
+        self.log = log
 
 
-    def run(self, queue):
-        for i in range(CARS_TO_PRODUCE):
+    def run(self):
+        for _ in range(CARS_TO_PRODUCE):
             # TODO Add you code here
             """
             create a car
             place the car on the queue
             signal the dealer that there is a car on the queue
             """
-            queue.put(Car())
-        queue.put("bye forever")
-        # signal the dealer that there there are not more cars
-        pass
+            c= Car.gen_random()
+            self.space_available.acquire()  # Wait for space in the queue
+            self.queue.put(c)  # Add the car to the queue
+            self.cars_available.release()  # Signal that a car is available
+            self.log.write(f"Factory produced: {c}")
+        # Signal the dealer that there there are not more cars
+        self.queue.put("bye forever")
+        self.cars_available.release()  # Ensure the dealer doesn't block on the final signal
+        self.log.write("Factory: Production complete!")
 
 
 class Dealer(threading.Thread):
     """ This is a dealer that receives cars """
 
-    def __init__(self):
+    def __init__(self, queue, space_available, cars_available, log, queue_stats):
         # TODO, you need to add arguments that pass all of data that 1 Dealer needs
         # to sell a car
-        pass
+        super().__init__()
+        self.queue = queue
+        self.space_available = space_available
+        self.cars_available = cars_available
+        self.log = log
+        self.queue_stats = queue_stats
 
-    def run(self, queue):
+    def run(self):
         while True:
             # TODO Add your code here
             """
             take the car from the queue
             signal the factory that there is an empty slot in the queue
             """
+            self.cars_available.acquire()  # Wait for a car to be available
+            c = self.queue.get()  # Get the car from the queue
+            self.space_available.release()
+            if c == "bye forever":
+                self.log.write("Dealer: Received termination signal.")
+                break
+            self.queue_stats[self.queue.size()] += 1
+            self.log.write(f"Dealer sold: {c}")
             # Sleep a little after selling a car
             # Last statement in this for loop - don't change
             time.sleep(random.random() / (SLEEP_REDUCE_FACTOR))
+            
 
 
 
@@ -125,29 +173,32 @@ def main():
     # TODO Create queue251 
     # TODO Create lock(s) ?
 
-    sf = threading.Semaphore(MAX_QUEUE_SIZE)
-    sd = threading.Semaphore(MAX_QUEUE_SIZE)
-    q = Queue251
+    # sem = threading.Semaphore(MAX_QUEUE_SIZE)
+    space_available = threading.Semaphore(MAX_QUEUE_SIZE)  # Starts with MAX_QUEUE_SIZE spaces
+    cars_available = threading.Semaphore(0)  # Starts with 0 cars available
+    queue = Queue251()
 
     # This tracks the length of the car queue during receiving cars by the dealership
     # i.e., update this list each time the dealer receives a car
     queue_stats = [0] * MAX_QUEUE_SIZE
 
     # TODO create your one factory
-    twenty_thousand_possible_cars_ten_at_a_time = Factory()
+    twenty_thousand_possible_cars_ten_at_a_time = Factory(queue, space_available, cars_available, log)
     # TODO create your one dealership
-    the_most_eclectic_ten_car_dealership_on_planet_earth = Dealer()
+    the_most_eclectic_ten_car_dealership_on_planet_earth = Dealer(queue, space_available, cars_available, log, queue_stats)
     log.start_timer()
 
     # TODO Start factory and dealership
-    twenty_thousand_possible_cars_ten_at_a_time.run(q)
+    twenty_thousand_possible_cars_ten_at_a_time.start()
+    the_most_eclectic_ten_car_dealership_on_planet_earth.start()
     # TODO Wait for factory and dealership to complete
-    the_most_eclectic_ten_car_dealership_on_planet_earth.run(q)
-    log.stop_timer(f'All {sum(queue_stats)} have been created')
+    twenty_thousand_possible_cars_ten_at_a_time.join()
+    the_most_eclectic_ten_car_dealership_on_planet_earth.join()
+    log.stop_timer(f'All {sum(queue_stats)} have been created and sold.')
 
-    xaxis = [i for i in range(0, MAX_QUEUE_SIZE)]
+    xaxis = [i + 1 for i in range(0, MAX_QUEUE_SIZE)] # i + 1 because the range is 1-10, not 0-9.
     plot = Plots()
-    plot.bar(xaxis, queue_stats, title=f'{sum(queue_stats)} Produced: Count VS Queue Size', x_label='Queue Size', y_label='Count', filename='Production count vs queue size.png')
+    plot.bar(xaxis, queue_stats, title=f'{sum(queue_stats)} Produced: Count VS Queue Size', x_label='Queue Size', y_label='Count', filename=f'Production count vs queue size-{time.strftime('%H%M%S')}.png')
 
 
 
