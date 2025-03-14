3c8dea7b884c9bf677cd1722dd171870fec3c4d4
Tyler Bartle <tman.bartleboy1@gmail.com>
2025-03-13 20:43:19 -0600

    Completed W11 Prove

--- lesson_11/prove/prove.py
+++ lesson_11/prove/prove.py
@@ -37,7 +37,7 @@ def guest_partying(id, count):
     print(f'Guest: {id}, count = {count}')
     time.sleep(random.uniform(0, 1))
 
-def cleaner():
+def cleaner(id, room_empty, cleaned_count):
     """
     do the following for TIME seconds
         cleaner will wait to try to clean the room (cleaner_waiting())
@@ -46,9 +46,17 @@ def cleaner():
         Take some time cleaning (cleaner_cleaning())
         display message STOPPING_CLEANING_MESSAGE
     """
-    pass
+    end_time = time.time() + TIME
+    while time.time() < end_time:
+        cleaner_waiting(id)
+        room_empty.acquire()  # Wait until no guests are in the room.
+        print(STARTING_CLEANING_MESSAGE)
+        cleaner_cleaning(id)
+        print(STOPPING_CLEANING_MESSAGE)
+        cleaned_count.value += 1
+        room_empty.release()  # Allow guests to enter.
 
-def guest():
+def guest(id, room_empty, guest_lock, guest_count, party_count, guest_semaphore):
     """
     do the following for TIME seconds
         guest will wait to try to get access to the room (guest_waiting())
@@ -57,17 +65,59 @@ def guest():
         Take some time partying (call guest_partying())
         display message STOPPING_PARTY_MESSAGE if the guest is the last one leaving in the room
     """
-    pass
+    end_time = time.time() + TIME
+    while time.time() < end_time:
+        guest_waiting(id)
+        guest_semaphore.acquire()  # Limit the number of guests that can be partying.
+        
+        # Begin critical section to update guest count.
+        with guest_lock:
+            guest_count.value += 1
+            if guest_count.value == 1:
+                room_empty.acquire()  # First guest blocks the cleaners.
+                print(STARTING_PARTY_MESSAGE)
+                party_count.value += 1
+        # End critical section.
+        
+        guest_partying(id, guest_count.value)
+        
+        # Begin critical section for leaving.
+        with guest_lock:
+            guest_count.value -= 1
+            if guest_count.value == 0:
+                print(STOPPING_PARTY_MESSAGE)
+                room_empty.release()  # Last guest leaves and allows cleaners in.
+        # End critical section.
+        
+        guest_semaphore.release()
 
 def main():
     # Start time of the running of the program. 
     start_time = time.time()
 
     # TODO - add any variables, data structures, processes you need
+    room_empty = mp.Semaphore(1) # Acts as a mutex for cleaning (only available when room is empty)
+    guest_lock = mp.Lock()       # Protects guest_count updates.
+    cleaned_count = mp.Value('i', 0)
+    guest_count = mp.Value('i', 0)
+    party_count = mp.Value('i', 0)
+    guest_semaphore = mp.Semaphore(HOTEL_GUESTS)
     # TODO - add any arguments to cleaner() and guest() that you need
+    cleaners = [mp.Process(target=cleaner, args=(i, room_empty, cleaned_count)) for i in range(CLEANING_STAFF)]
+    for c in cleaners:
+        c.start()
+
+    guests = [mp.Process(target=guest, args=(i, room_empty, guest_lock, guest_count, party_count, guest_semaphore)) for i in range(HOTEL_GUESTS)]
+    for g in guests:
+        g.start()
+
+    for c in cleaners:
+        c.join()
+    for g in guests:
+        g.join()
 
     # Results
-    print(f'Room was cleaned {cleaned_count} times, there were {party_count} parties')
+    print(f'Room was cleaned {cleaned_count.value} times, there were {party_count.value} parties')
 
 
 if __name__ == '__main__':
